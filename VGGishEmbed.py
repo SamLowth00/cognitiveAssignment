bc85c2e37d08238891aba54429146fe78856491a
samlowth00 <slowth1@sheffield.ac.uk>
2022-05-05 17:04:55 +0100

    added guess data

--- VGGishEmbed.py
+++ VGGishEmbed.py
@@ -38,7 +38,7 @@ strokeEmbed = model.forward(strokeFile)
 backgroundEmbed = model.forward(backgroundFile)
 
 guessEmbed = model.forward(guessFile)
-
+numpyGuessEmbed = [ item.detach().numpy() for item in guessEmbed]
 #np conversion
 numpyPinchEmbed = [ item.detach().numpy() for item in pinchEmbed]
 numpyBackgroundEmbed = [ item.detach().numpy() for item in backgroundEmbed]
@@ -62,18 +62,18 @@ clf = svm.SVC(kernel='linear')
 clf.fit(fullEmbed,numpyTarget)
 
 #set up a test 1 second
-testPinch = "EarPinchTestData2.wav"
-testPinchEmbed = model.forward(testPinch)
-numpyTestPinchEmbed = [ item.detach().numpy() for item in testPinchEmbed]
+#testPinch = "EarPinchTestData2.wav"
+#testPinchEmbed = model.forward(testPinch)
+#numpyTestPinchEmbed = [ item.detach().numpy() for item in testPinchEmbed]
 
-testStroke = "EarStrokeTestData2.wav"
-testStrokeEmbed = model.forward(testStroke)
-numpyTestStrokeEmbed = [ item.detach().numpy() for item in testStrokeEmbed]
+#testStroke = "EarStrokeTestData2.wav"
+#testStrokeEmbed = model.forward(testStroke)
+#numpyTestStrokeEmbed = [ item.detach().numpy() for item in testStrokeEmbed]
 
 # Optimisation (3/3)
-numpyTestPinchEmbed = optimisation(numpyTestPinchEmbed)
-numpyTestStrokeEmbed = optimisation(numpyTestStrokeEmbed)
-
+#numpyTestPinchEmbed = optimisation(numpyTestPinchEmbed)
+#numpyTestStrokeEmbed = optimisation(numpyTestStrokeEmbed)
+numpyGuessEmbed = optimisation(numpyGuessEmbed)
 # print(len(numpyTestEmbed))
 
 # testValue = (numpyTestEmbed[15])
@@ -93,19 +93,19 @@ for item in numpyTestPinchEmbed:
     testResult = clf.predict(testValue)
     overallPinchTest = overallPinchTest + testResult[0]
 overallPinchTest = overallPinchTest / len(numpyTestPinchEmbed)
-print("Closer to 0 = Pinch")
-print("Closer to 1 = Nothing")
-print("Closer to 2 = Stroke")
-print("The average value for the pinch test data is:")
-print(overallPinchTest)
+#print("Closer to 0 = Pinch")
+#print("Closer to 2 = Stroke")
+#print("The average value for the pinch test data is:")
+#print(overallPinchTest)
 
-for item in numpyBackgroundEmbed:
+for item in numpyGuessEmbed:
     testValue = (item)
     testValue = testValue.reshape(1,-1)
 
     testResult = clf.predict(testValue)
     print(testResult[0])
     overallGuess = overallGuess + testResult[0]
+
 overallGuess = overallGuess / len(numpyGuessEmbed)
 print("guess value is")
 print(overallGuess)
